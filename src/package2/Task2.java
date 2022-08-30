c1df229c77110b13bf1c7f92172f80a54dfcd5f8
Anahit Aghabekyan <anahit.aghabekyan91@gmail.com>
2022-08-30 17:53:28 -0400

    Task2 class created

---
+++ src/package2/Task2.java
@@ -0,0 +1,12 @@
+package package2;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
